f3bbf6070b75dac9c9b08e2175899add51a8b19e
Anderson Tchuy <andersontchuy@hotmail.com>
2020-07-16 12:03:22 -0300

    Refatorando código

--- app.py
+++ app.py
@@ -39,8 +39,5 @@ def prever_evasao_aluno(matricula):
     return jsonify(result), 200
 
 if __name__ == "__main__":
-    app.run(host='127.0.0.1', port=5000, debug=True)
-
-# if __name__ == "__main__":
-#     port = int(os.environ.get("PORT", 5000))
-#     app.run(host='0.0.0.0', port=port)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
